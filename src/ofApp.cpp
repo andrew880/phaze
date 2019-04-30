bdb0f84ca39cc3202222af0c9a9f6a06714f7fcf
andrew <andrewlin1226@gmail.com>
2019-04-30 14:42:55 -0500

    organize generate functions

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -27,8 +27,9 @@ void ofApp::setup() {
 	update();
 
 	playerInit();
-	portalStartInit();
 	mapInit(map);
+
+	update();
 }
 
 //--------------------------------------------------------------
@@ -74,9 +75,6 @@ void ofApp::draw() {
 	ofSetHexColor(0xEEEEEE);
 	player->draw();
 
-	ofSetHexColor(0x3E00DB);
-	portalStart->draw();
-
 	// draw current ground
 	box2dArr[worldIndex * 2 + (blue ? 0 : 1)].drawGround();
 
@@ -104,17 +102,8 @@ void ofApp::playerInit() {
 	player = shared_ptr<ofxBox2dCircle>(new ofxBox2dCircle);
 	player.get()->setPhysics(8.0, 0.0, 1);
 	player.get()->setup((box2dArr[worldIndex * 2 + (blue ? 0 : 1)]).getWorld(), kwidth / 2, kheight / 2, 25);
-	player.get()->setFixedRotation(true);
+	//player.get()->setFixedRotation(true);
 	player.get()->setData("p");
-}
-
-//--------------------------------------------------------------
-void ofApp::portalStartInit()
-{
-	portalStart = shared_ptr<ofxBox2dCircle>(new ofxBox2dCircle);
-	portalStart.get()->setup((box2dArr[worldIndex * 2 + (blue ? 0 : 1)]).getWorld(), kwidth - kwidth / 8, kheight - kheight / 8, 25);
-	portalStart.get()->setFixedRotation(true);
-	portalStart.get()->setData("t");
 }
 
 // -blue || +red
@@ -126,26 +115,72 @@ void ofApp::mapInit(vector<vector<char*>> map)
 				int x = j * kwidth / kMapHeight;
 				int y = i * kheight / kMapWidth;
 				if (map[k][i][j] == '-') {
-					boxesB.push_back(shared_ptr<ofxBox2dRect>(new ofxBox2dRect));
-					boxesB.back().get()->setup(box2dArr[k * 2].getWorld(), x, y, 20, 5);
-					boxesB.back().get()->setData(box2dArr[k * 2].getWorld());
-				}
-				else if (map[k][i][j] == '+') {
-					boxesR.push_back(shared_ptr<ofxBox2dRect>(new ofxBox2dRect));
-					boxesR.back().get()->setup(box2dArr[k * 2 + 1].getWorld(), x, y, 20, 5);
-					boxesR.back().get()->setData(box2dArr[k * 2 + 1].getWorld());
-				}
-				else if (map[k][i][j] == '=') {
-					portals.push_back(shared_ptr<ofxBox2dCircle>(new ofxBox2dCircle));
-					portals.back().get()->setup(box2dArr[k * 2 + 1].getWorld(), x, y, 35);
-					portals.back().get()->setData(box2dArr[k * 2 + 1].getWorld());
-					portals.push_back(shared_ptr<ofxBox2dCircle>(new ofxBox2dCircle));
-					portals.back().get()->setup(box2dArr[k * 2].getWorld(), x, y, 35);
-					portals.back().get()->setData(box2dArr[k * 2].getWorld());
+					boxBHCreate(k, x, y);
+				} else if (map[k][i][j] == '+') {
+					boxRHCreate(k, x, y);
+				} else if(map[k][i][j] == '/') {
+					boxBVCreate(k, x, y);
+				} else if (map[k][i][j] == '*') {
+					boxRVCreate(k, x, y);
+				} else if (map[k][i][j] == '=') {
+					portalCreate(x, y, k, 0);
+					portalCreate(x, y, k, 1);
+				} else if (map[k][i][j] == '_') {
+					boxBHCreate(k, x, y);
+					boxRHCreate(k, x, y);
+				} else if (map[k][i][j] == '|') {
+					boxBVCreate(k, x, y);
+					boxRVCreate(k, x, y);
 				}
 			}
 		}
 	}
+}
+
+void ofApp::boxBHCreate(int k, int x, int y)
+{
+	boxesB.push_back(shared_ptr<ofxBox2dRect>(new ofxBox2dRect));
+	boxesB.back().get()->setFriction(1);
+	boxesB.back().get()->setup(box2dArr[k * 2].getWorld(), x, y, kboxw, kboxh);
+	boxesB.back().get()->setData(box2dArr[k * 2].getWorld());
+}
+
+void ofApp::boxRHCreate(int k, int x, int y)
+{
+	boxesR.push_back(shared_ptr<ofxBox2dRect>(new ofxBox2dRect));
+	boxesR.back().get()->setFriction(1);
+	boxesR.back().get()->setup(box2dArr[k * 2 + 1].getWorld(), x, y, kboxw, kboxh);
+	boxesR.back().get()->setData(box2dArr[k * 2 + 1].getWorld());
+}
+
+void ofApp::boxBVCreate(int k, int x, int y)
+{
+	boxesB.push_back(shared_ptr<ofxBox2dRect>(new ofxBox2dRect));
+	boxesB.back().get()->setFriction(1);
+	boxesB.back().get()->setup(box2dArr[k * 2].getWorld(), x, y, kboxw, kboxh);
+	boxesB.back().get()->setData(box2dArr[k * 2].getWorld());
+}
+
+void ofApp::boxRVCreate(int k, int x, int y)
+{
+	boxesR.push_back(shared_ptr<ofxBox2dRect>(new ofxBox2dRect));
+	boxesR.back().get()->setFriction(1);
+	boxesR.back().get()->setup(box2dArr[k * 2 + 1].getWorld(), x, y, kboxw, kboxh);
+	boxesR.back().get()->setData(box2dArr[k * 2 + 1].getWorld());
+}
+
+void ofApp::portalCreate(int x, int y, int k, int i)
+{
+	portals.push_back(shared_ptr<ofxBox2dCircle>(new ofxBox2dCircle));
+	portals.back.get()->setup((box2dArr[k * 2 + i]).getWorld(), x, y, 35);
+	portals.back.get()->setFixedRotation(true);
+}
+
+void ofApp::playerUpdate()
+{
+	ofVec2f v = player->getVelocity();
+	player->setup((box2dArr[worldIndex * 2 + (blue ? 0 : 1)]).getWorld(), player->getPosition(), 25);
+	player->setVelocity(v);
 }
 
 //--------------------------------------------------------------
@@ -156,22 +191,6 @@ void ofApp::keyPressed(int key) {
 	}
 	if (key == 'o') {
 		worldIndex--;
-	}
-	if (key == 'b') { 
-		float w = ofRandom(4, 20);
-		float h = ofRandom(4, 20);
-		boxesB.push_back(shared_ptr<ofxBox2dRect>(new ofxBox2dRect));
-		//boxes.back().get()->setPhysics(3.0, 0.53, 0.1);
-		boxesB.back().get()->setup(box2dArr[worldIndex * 2].getWorld(), mouseX, mouseY, w, h);
-		boxesB.back().get()->setData(box2dArr[worldIndex * 2].getWorld());
-	}
-	if (key == 'r') {
-		float w = ofRandom(4, 20); 
-		float h = ofRandom(4, 20);
-		boxesR.push_back(shared_ptr<ofxBox2dRect>(new ofxBox2dRect));
-		//boxes.back().get()->setPhysics(3.0, 0.53, 0.1);
-		boxesR.back().get()->setup(box2dArr[worldIndex * 2 + 1].getWorld(), mouseX, mouseY, w, h);
-		boxesR.back().get()->setData(box2dArr[worldIndex * 2 + 1].getWorld());
 	}
 
 	if (key == 't') ofToggleFullscreen();
@@ -193,18 +212,9 @@ void ofApp::keyPressed(int key) {
 	
 	//change dimension
 	if (key == 'q' && !shifted) {
-		//shifted = true;
+		shifted = true;
 		blue = !blue;
-
-		ofVec2f v = player->getVelocity();
-		ofxBox2dCircle temp = *player;
-		player->setup((box2dArr[worldIndex * 2 + (blue ? 0 : 1)]).getWorld(), player->getPosition(), 25);
-		player->setVelocity(v);
-		b2Filter f = b2Filter();
-		f.groupIndex = (blue ? 0 : 1);
-		player->setFilterData(f);
-		//portalStart->setup((box2dArr[worldIndex * 2 + (blue ? 0 : 1)]).getWorld(), portalStart->getPosition(), 25);
-		grounded = false;
+		playerUpdate();
 	}
 }
 
@@ -256,7 +266,8 @@ void ofApp::gotMessage(ofMessage msg) {
 void ofApp::contactStart(ofxBox2dContactArgs& e) {
 	if (e.a != NULL && e.b != NULL) {
 		s = "smt" + ofToString(e.a->GetUserData()) + ofToString(e.b->GetUserData());
-		if ((e.a->GetType() == b2Shape::e_circle || e.b->GetType() == b2Shape::e_circle)) {
+		if ((e.a->GetType() == b2Shape::e_circle && e.b->GetType() == b2Shape::e_polygon) || 
+			(e.b->GetType() == b2Shape::e_circle && e.a->GetType() == b2Shape::e_polygon)) {
 			grounded = true;
 			shifted = false;
 		}
@@ -264,7 +275,7 @@ void ofApp::contactStart(ofxBox2dContactArgs& e) {
 		if (e.a->GetType() == b2Shape::e_circle && e.b->GetType() == b2Shape::e_circle) {
 			if (worldIndex + 1 < kworldCount / 2) {
 				worldIndex += 1;
-				player->setup(box2dArr[worldIndex * 2 + (blue ? 0 : 1)].getWorld(), kwidth / 8, kheight - kheight / 8, 25);
+				playerUpdate();
 			} else if (worldIndex + 1 == kworldCount / 2) {
 				//worldIndex = 0;
 				//player->setup(box2dArr[worldIndex * 2 + (blue ? 0 : 1)].getWorld(), kwidth / 8, kheight - kheight / 8, 25);
